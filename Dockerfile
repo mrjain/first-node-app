637835c4320321febd982dc85e5342f943ed0ad5
Manish Jain <mrjain@gmail.com>
2019-09-27 18:12:08 +0530

    changes to app

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY package.json /app
 RUN npm install
 COPY . /app
-CMD ["npm", "start"]
+EXPOSE 3000
+CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const http = require('http');
 
-const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send('<h1>My Node App is listening on port 911</h1>');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World on Port 3000');
 });
 
-app.listen(911, () => {
-    console.log('My Node App is listening on port 911');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
